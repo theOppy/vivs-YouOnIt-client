b6a90c338f68639e2d3626bc16bf726249367481
Vivek <122638553+theOppy@users.noreply.github.com>
2023-07-07 18:52:38 +0530

    Update TweetReply.js

--- src/components/TweetReply.js
+++ src/components/TweetReply.js
@@ -4,7 +4,7 @@ import "../index.css";
 import { Link } from "react-router-dom";
 
 const TweetReply = () => {
-  const tweetID = window.location.href.slice(28, 70);
+  const tweetID = window.location.href.slice(38, 70);
 
   const [tweet, setTweet] = React.useState({});
 
